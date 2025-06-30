55882275c8e2c6992f1df573f3abf5023658ed81
charlys202 <dolorescarlos2003@gmail.com>
2025-06-29 21:54:12 -0600

    Add files via upload

---
+++ ProyAutomatas/src/Generadores/A_Lexico.bat
@@ -0,0 +1,6 @@
+SET JAVA_HOME="C:\Program Files\Java\jdk1.8.0_05\bin"
+SET PATH=%JAVA_HOME%;%PATH%
+SET CLASSPATH=%JAVA_HOME%;
+cd C:\Users\Mynor\OneDrive\Imágenes\sintactico\JflexyCupEjemplo\src\Analizadores
+java -jar C:\Users\Mynor\OneDrive\Imágenes\sintactico\cupppppp\JFlex.jar A_Lexico.jflex
+pause
