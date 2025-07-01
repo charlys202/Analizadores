c87f50f2849e68ad7a97228130f335b4c5945ac3
charlys202 <dolorescarlos2003@gmail.com>
2025-07-01 02:12:21 -0600

    Delete ProyAutomatas/src directory

--- ProyAutomatas/src/Generadores/A_Sintactico.bat
+++
@@ -1,6 +0,0 @@
-SET JAVA_HOME="C:\Program Files\Java\jdk1.8.0_05\bin"
-SET PATH=%JAVA_HOME%;%PATH%
-SET CLASSPATH=%JAVA_HOME%;
-cd C:\Users\Mynor\OneDrive\Imágenes\sintactico\JflexyCupEjemplo\src\Analizadores
-java -jar C:\Users\Mynor\OneDrive\Imágenes\sintactico\cupppppp\java-cup-11b.jar -parser analisis_sintactico -symbols Simbolos A_Sintactico.cup
-pause
